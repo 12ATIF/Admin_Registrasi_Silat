b05cdd62852e9429def7b6083c1469dd4d6f7fdd
12ATIF <latifali84759@gmail.com>
2026-04-16 13:30:51 +0700

    fix: tambah kolom role ke tabel pelatih

--- database/migrations/2025_05_15_014412_create_pelatih_table.php
+++ database/migrations/2025_05_15_014412_create_pelatih_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('is_active')->default(true); // Untuk fitur nonaktifkan
+            $table->string('role')->default('user');
             $table->timestamps();
         });
     }
